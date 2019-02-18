5d83d8539f925871c62a18bfca79d3811cd1f1a4
indyroode <38463347+indyroode@users.noreply.github.com>
2019-02-18 13:31:09 +0100

    Add files via upload

--- Code/shaders/fragshader.glsl
+++ Code/shaders/fragshader.glsl
@@ -1,10 +1,20 @@
 #version 330 core
 
-out vec4 fragColor;
+// Define constants
+#define M_PI 3.141593
 
-in vec3 interpolatedColor;
+// Specify the inputs to the fragment shader
+// These must have the same type and name!
+in vec3 vertColor;
+
+// Specify the Uniforms of the fragment shaders
+// uniform vec3 lightPosition; // for example
+
+// Specify the output of the fragment shader
+// Usually a vec4 describing a color (Red, Green, Blue, Alpha/Transparency)
+out vec4 fColor;
 
 void main()
 {
-    fragColor = vec4(interpolatedColor,1);
+    fColor = vec4(vertColor, 1.0);
 }
